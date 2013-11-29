a04a3d01de2733cb639c30537f13be5baa901a6a
vitorsouza <vitorsouza@gmail.com>
2013-11-28 22:51:15 -0200

    First commit with code imported from the Sigme project.
    The Sigme project (https://github.com/feees/Sigme) contained utilities
    for general Java, Java EE Web Profile and Java EE Full Profile projects.
    Since these utilities might be useful for projects in Nemo, they were
    transformed into their own modules. The idea is to make Maven
    dependencies out of them once I have a Maven2 repository set up
    somewhere.

---
+++ nemo-utils-jee-wp/src/main/java/br/ufes/inf/nemo/util/ejb3/domain/DomainObjectSupport_.java
@@ -0,0 +1,16 @@
+package br.ufes.inf.nemo.util.ejb3.domain;
+
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+/**
+ * Meta-model for the DomainObjectSupport domain class, which allows DAOs to perform programmatic queries using JPA2's
+ * Criteria API.
+ * 
+ * @author Vitor E. Silva Souza (vitorsouza@gmail.com)
+ * @see br.ufes.inf.nemo.util.ejb3.domain.DomainObjectSupport
+ */
+@StaticMetamodel(DomainObjectSupport.class)
+public class DomainObjectSupport_ {
+	public static volatile SingularAttribute<DomainObjectSupport, String> uuid;
+}
---
+++ nemo-utils-jee-wp/src/main/java/br/ufes/inf/nemo/util/people/persistence/StateDAO.java
@@ -0,0 +1,21 @@
+package br.ufes.inf.nemo.util.people.persistence;
+
+import javax.ejb.Local;
+
+import br.ufes.inf.nemo.util.ejb3.persistence.BaseDAO;
+import br.ufes.inf.nemo.util.people.domain.State;
+
+/**
+ * Interface for a DAO for objects of the State domain class.
+ * 
+ * Using a mini CRUD framework for EJB3, basic DAO operation definitions are inherited from the superclass, whereas
+ * operations that are specific to the managed domain class (if any) are specified in this class.
+ * 
+ * <i>This class is part of the Engenho de Software "Legal Entity" mini framework for EJB3 (Java EE 6).</i>
+ * 
+ * @author Vitor E. Silva Souza (vitorsouza@gmail.com)
+ * @see br.ufes.inf.nemo.util.people.persistence.State
+ * @see br.ufes.inf.nemo.util.ejb3.persistence.BaseDAO
+ */
+@Local
+public interface StateDAO extends BaseDAO<State> {}
